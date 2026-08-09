c7a3537e220ecb21037c35e0e41c89497bcba289
erickMartinsSilva <erickmsilva19@gmail.com>
2026-08-09 10:12:10 -0300

    refactor(exercicios/linkedlist): ex02

--- exercicios/linkedlist/ex02.cpp
+++ exercicios/linkedlist/ex02.cpp
@@ -1,26 +1,40 @@
 // Função que percorre uma lista simplesmente encadeada e retorna quantos elementos são maiores que um N informado pelo usuário
 #include <iostream>
+#include "simply_linked_list.hpp"
 
 using namespace std;
 
-struct no {
-    int info;
-    struct no* prox;
-};
+int linked_list_amount_numbers_higher_than(NodePtr head, int number) {
+    NodePtr aux = head; 
+    int amount = 0;
 
-typedef struct no* noPtr;
-
-noPtr topo = NULL;
-
-int listaNumerosMaiorQueN() {
-    noPtr aux = topo; int cntd = 0, n;
-    cout << "Insira um numero: ";
-    cin >> n;
     while(aux != NULL) {
-        if(aux->info > n) {
-            cntd++;
+        if(aux->data > number) {
+            amount++;
         }
-        aux = aux->prox;
+        aux = aux->next;
     }
-    return cntd;
+    return amount;
+}
+
+void test_linked_list_amount_numbers_higher_than() {
+    NodePtr head = NULL, tail = NULL;
+
+    add_to_simply_linked_list(&head, &tail, 1);
+    add_to_simply_linked_list(&head, &tail, 2);
+    add_to_simply_linked_list(&head, &tail, 3);
+    add_to_simply_linked_list(&head, &tail, 4);
+    add_to_simply_linked_list(&head, &tail, 5);
+
+    int result = linked_list_amount_numbers_higher_than(head, 2);
+    bool passed = result == 3;
+    if(!passed) {
+        printf("Test failed. Expected output: 3, obtained output: %d\n", result);
+        return;
+    }
+    printf("Passed\n");
+}
+
+int main() {
+    test_linked_list_amount_numbers_higher_than();
 }
