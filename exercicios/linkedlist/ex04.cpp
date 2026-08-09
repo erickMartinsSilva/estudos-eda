15676eaaa6b02646a72154d5afbe299fd86bdd83
erickMartinsSilva <erickmsilva19@gmail.com>
2026-08-09 15:29:37 -0300

    refactor(exercicios/linkedlist): ex04

--- exercicios/linkedlist/ex04.cpp
+++ exercicios/linkedlist/ex04.cpp
@@ -1,30 +1,44 @@
 // Função que recebe uma lista encadeada e um número como parâmetros e remove todas as incidências do número da lista, imprimindo-a
 #include <iostream>
+#include "simply_linked_list.hpp"
 
 using namespace std;
 
-struct no {
-    int info;
-    struct no* prox;
-};
+void removeFromLista(NodePtr *topo, int n) {
+    NodePtr aux = *topo, ant = NULL;
+    bool found = false;
 
-typedef struct no* noPtr;
-
-void removeFromLista(noPtr topo, int n) {
-    noPtr aux, ant, aux2;
-    aux = topo;
-    while(aux != NULL) {
-        ant = aux;
-        if(aux->info == n) {
-            aux2 = aux;
-            ant->prox = aux2->prox;
-            delete(aux2);
+    while(!found && aux != NULL) {
+        found = aux->data == n;
+        if(!found) {
+            ant = aux;
+            aux = aux->next;
         }
-        aux = aux->prox;
     }
-    aux = topo;
-    while(aux != NULL) {
-        cout << aux->info;
-        aux = aux->prox;
+    if(!found) return;
+
+    if(aux == *topo) {
+        *topo = aux->next;
+    } else {
+        ant->next = aux->next;
     }
+    aux->next = NULL;
+    delete aux;
+}
+
+void test_removeFromLista() {
+    NodePtr head = NULL, tail = NULL;
+
+    add_to_simply_linked_list(&head, &tail, 1);
+    add_to_simply_linked_list(&head, &tail, 2);
+    add_to_simply_linked_list(&head, &tail, 3);
+
+    removeFromLista(&head, 2);
+
+    printf("Result: ");
+    print_simply_linked_list(head);
+}
+
+int main() {
+    test_removeFromLista();
 }
